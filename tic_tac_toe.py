571d84d3f00df62fa9c776174bafca9a8edcb9d1
RickLarrabee <lar21035@byui.edu>
2022-01-14 20:18:24 -0500

    Newest version

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,23 +1,37 @@
 # File Name: tic_tac_toe.py
 # Author: Rick Larrabee
 
+"""This tic tac toe game shows the ability to utilize functions to make
+simple game in Python"""
+
+# Main function that runs the game 
 def main ():
     print ('Welcome to the great game of tic tac toe')
     print ('please enter the player information.')
-    player_1 = input('Enter the name for player 1 (will be X): ')
-    player_2 = input('Enter the name for player 2 (will be O): ')
+    player_1 = "Player 1"
+    player_2 = "Player 2"
     current_player = player_1
     current_play = 0
     board_squares = set_board ()
     while not (determine_winner(board_squares) or current_play == 9):
         print_board(board_squares)
         player_move(current_play, current_player, player_1, player_2, board_squares)
+        print('\n')
         current_play = current_play + 1
-        print(current_play)
-        current_player = switch_player (current_player, player_1, player_2)
-    determine_draw (current_play)
-    print_board
+        if not (determine_winner(board_squares) or current_play == 9):
+            current_player = switch_player (current_player, player_1, player_2)
+    print_board(board_squares)
+    if current_play == 9:
+        print("It is a draw")
+    else:
+        print (f"Congratulaitons {current_player} you won!")
+    print ('\n')
+    if play_again() == "Y":
+        main()
+    else:
+        print("Thank you for playing.")
 
+# Function that sets the values for the board in a list
 def set_board ():
     board_squares = []
 
@@ -26,6 +40,7 @@ def set_board ():
 
     return board_squares
 
+# Function that prints the board for the players to see.
 def print_board (squares):
     
     print ('   |   |   ')
@@ -40,6 +55,7 @@ def print_board (squares):
     print (f' {squares[6]} | {squares[7]} | {squares[8]}')
     print ('   |   |   ')
 
+# Function that will look at the current board and determine if there is a winner.
 def determine_winner (squares):
     return (squares[0] == squares[1] ==squares[2] or
             squares[3] == squares[4] ==squares[5] or
@@ -50,11 +66,7 @@ def determine_winner (squares):
             squares[1] == squares[4] ==squares[7] or
             squares[2] == squares[5] ==squares[8])
 
-def determine_draw (current_play):
-    if current_play == 9:
-        print('This game is a draw.')
-
-
+# Function allows the current player to select the square they want and determines if the move is valid.
 def player_move (current, current_player, player_1, player_2, squares):
     while True:
         try:
@@ -72,13 +84,26 @@ def player_move (current, current_player, player_1, player_2, squares):
         except ValueError:
             print ('wrong input, please try again.')
 
+
+# Function to change the player for the next move.
 def switch_player (current_player, player_1, player_2):
     if current_player == player_1:
         current_player = player_2
     elif current_player == player_2:
         current_player = player_1
     return current_player
-        
+
+# Function to ask if the players want another game.
+def play_again ():
+    while True:
+        try:
+            new_game=input ("Do you want to play again (Y or N): ")
+            if new_game.upper() not in ("Y", "N"):
+                raise ValueError
+            break
+        except ValueError:
+            print ("You must choose Y or N")
+    return new_game.upper()
 
 if __name__ == "__main__":
     main()
